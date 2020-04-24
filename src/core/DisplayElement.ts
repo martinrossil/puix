a60604d18b7b86fb95591589025e3db047c145e0
Martin Rossil <martinrossil@gmail.com>
2020-04-24 11:36:08 +0200

    Added opacity.

--- src/core/DisplayElement.ts
+++ src/core/DisplayElement.ts
@@ -17,5 +17,16 @@ export default class DisplayElement extends SizeElement implements IDisplayEleme
     public get backgroundColor(): string {
         return this._backgroundColor;
     }
+
+    private _opacity = 1;
+
+    public set opacity(value: number) {
+        this._opacity = value;
+        this.style.opacity = value.toString();
+    }
+
+    public get opacity(): number {
+        return this._opacity;
+    }
 }
 customElements.define('display-element', DisplayElement);
