43865a68d77fe512bfc8cc55a95c1b3ef2a3821d
Martin Rossil <martinrossil@gmail.com>
2020-04-24 11:07:02 +0200

    Added more interfaces and elements to index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import IEventDispatcher from './interfaces/IEventDispatcher';
+import ILifeCycleElement from './interfaces/ILifeCycleElement';
+import IPositionElement from './interfaces/IPositionElement';
+import ISizeElement from './interfaces/ISizeElement';
 import EventDispatcherElement from './core/EventDispatcherElement';
+import LifeCycleElement from './core/LifeCycleElement';
+import PositionElement from './core/PositionElement';
+import SizeElement from './core/SizeElement';
 
 export {
     IEventDispatcher,
-    EventDispatcherElement
+    ILifeCycleElement,
+    IPositionElement,
+    ISizeElement,
+    EventDispatcherElement,
+    LifeCycleElement,
+    PositionElement,
+    SizeElement
 }
