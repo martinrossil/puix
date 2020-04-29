43a9a0c3f59526cdb3f394c3a0fe021615d7f92a
Martin Rossil <martinrossil@gmail.com>
2020-04-29 14:44:18 +0200

    Updated index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
+import IDisplayContainer from './interfaces/IDisplayContainer';
 import IDisplayElement from './interfaces/IDisplayElement';
 import IEventDispatcher from './interfaces/IEventDispatcher';
 import ILifeCycleElement from './interfaces/ILifeCycleElement';
 import IPositionElement from './interfaces/IPositionElement';
 import ISizeElement from './interfaces/ISizeElement';
 
+import DisplayContainer from './core/DisplayContainer';
 import DisplayElement from './core/DisplayElement';
 import EventDispatcherElement from './core/EventDispatcherElement';
 import LifeCycleElement from './core/LifeCycleElement';
 import PositionElement from './core/PositionElement';
 import SizeElement from './core/SizeElement';
+import Events from './enums/Events';
 
 export {
+    IDisplayContainer,
     IDisplayElement,
     IEventDispatcher,
     ILifeCycleElement,
     IPositionElement,
     ISizeElement,
+    DisplayContainer,
     DisplayElement,
     EventDispatcherElement,
     LifeCycleElement,
     PositionElement,
-    SizeElement
+    SizeElement,
+    Events
 }
