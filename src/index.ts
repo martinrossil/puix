7e91cb49c60cee96ccfde67d46067cf158ae92da
Martin Rossil <martinrossil@gmail.com>
2020-05-03 20:44:32 +0200

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,50 @@
+import IAnchorLayoutData from './interfaces/IAnchorLayoutData';
 import IDisplayContainer from './interfaces/IDisplayContainer';
 import IDisplayElement from './interfaces/IDisplayElement';
 import IEventDispatcher from './interfaces/IEventDispatcher';
+import ILayout from './interfaces/ILayout';
+import ILayoutContainer from './interfaces/ILayoutContainer';
+import ILayoutData from './interfaces/ILayoutData';
 import ILifeCycleElement from './interfaces/ILifeCycleElement';
 import IPositionElement from './interfaces/IPositionElement';
 import ISizeElement from './interfaces/ISizeElement';
 
 import DisplayContainer from './core/DisplayContainer';
 import DisplayElement from './core/DisplayElement';
+import EventDispatcher from './core/EventDispatcher';
 import EventDispatcherElement from './core/EventDispatcherElement';
+import LayoutContainer from './core/LayoutContainer';
 import LifeCycleElement from './core/LifeCycleElement';
 import PositionElement from './core/PositionElement';
 import SizeElement from './core/SizeElement';
 import Events from './enums/Events';
+import AnchorLayout from './layouts/AnchorLayout';
+import AnchorLayoutData from './layouts/AnchorLayoutData';
+import BaseLayout from './layouts/BaseLayout';
+import LayoutData from './layouts/LayoutData';
 
 export {
+    IAnchorLayoutData,
     IDisplayContainer,
     IDisplayElement,
     IEventDispatcher,
+    ILayout,
+    ILayoutContainer,
+    ILayoutData,
     ILifeCycleElement,
     IPositionElement,
     ISizeElement,
     DisplayContainer,
     DisplayElement,
+    EventDispatcher,
     EventDispatcherElement,
+    LayoutContainer,
     LifeCycleElement,
     PositionElement,
     SizeElement,
-    Events
+    Events,
+    AnchorLayout,
+    AnchorLayoutData,
+    BaseLayout,
+    LayoutData
 }
