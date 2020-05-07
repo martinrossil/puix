9655f7229742e4bece61a43be2a0964de4472614
Martin Rossil <martinrossil@gmail.com>
2020-05-07 10:19:41 +0200

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,97 @@
+// containers
 import ApplicationElement from './containers/ApplicationElement';
 import DisplayContainer from './containers/DisplayContainer';
+import LayoutContainer from './containers/LayoutContainer';
+
+// core
 import DisplayElement from './core/DisplayElement';
 import EventDispatcher from './core/EventDispatcher';
 import EventDispatcherElement from './core/EventDispatcherElement';
-import LayoutContainer from './containers/LayoutContainer';
 import LifeCycleElement from './core/LifeCycleElement';
 import PositionElement from './core/PositionElement';
 import SizeElement from './core/SizeElement';
 
+// enums
 import Events from './enums/Events';
+import FontWeight from './enums/FontWeight';
+import Overflow from './enums/Overflow';
 import Styles from './enums/Styles';
+import TextOverflow from './enums/TextOverflow';
 import Values from './enums/Values';
+import WhiteSpace from './enums/WhiteSpace';
 
+// interfaces
 import IAnchorLayoutData from './interfaces/IAnchorLayoutData';
 import IDisplayContainer from './interfaces/IDisplayContainer';
 import IDisplayElement from './interfaces/IDisplayElement';
 import IEventDispatcher from './interfaces/IEventDispatcher';
+import IFontDescription from './interfaces/IFontDescription';
 import ILayout from './interfaces/ILayout';
 import ILayoutContainer from './interfaces/ILayoutContainer';
 import ILayoutData from './interfaces/ILayoutData';
 import ILifeCycleElement from './interfaces/ILifeCycleElement';
 import IPositionElement from './interfaces/IPositionElement';
 import ISizeElement from './interfaces/ISizeElement';
+import ITextElement from './interfaces/ITextElement';
+import ITextRenderer from './interfaces/ITextRenderer';
 
+// layouts
 import AnchorLayout from './layouts/AnchorLayout';
 import AnchorLayoutData from './layouts/AnchorLayoutData';
 import BaseLayout from './layouts/BaseLayout';
 import LayoutData from './layouts/LayoutData';
 
+// text
+import FontDescription from './text/FontDescription';
+import TextElement from './text/TextElement';
+import TextRenderer from './text/TextRenderer';
+
 export {
+    // containers
     ApplicationElement,
     DisplayContainer,
+    LayoutContainer,
+
+    // core
     DisplayElement,
     EventDispatcher,
     EventDispatcherElement,
-    LayoutContainer,
     LifeCycleElement,
     PositionElement,
     SizeElement,
 
+    // enums
     Events,
+    FontWeight,
+    Overflow,
     Styles,
+    TextOverflow,
     Values,
+    WhiteSpace,
 
+    // interfaces
     IAnchorLayoutData,
     IDisplayContainer,
     IDisplayElement,
     IEventDispatcher,
+    IFontDescription,
     ILayout,
     ILayoutContainer,
     ILayoutData,
     ILifeCycleElement,
     IPositionElement,
     ISizeElement,
+    ITextElement,
+    ITextRenderer,
 
+    // layouts
     AnchorLayout,
     AnchorLayoutData,
     BaseLayout,
-    LayoutData
+    LayoutData,
+
+    // text
+    FontDescription,
+    TextElement,
+    TextRenderer
 }
