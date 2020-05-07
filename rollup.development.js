47f36a3ec1e26e5fc625a8ce933f60fd88595af5
Martin Rossil <martinrossil@gmail.com>
2020-05-07 13:27:43 +0200

    Added fonts to dev environment.

--- rollup.development.js
+++ rollup.development.js
@@ -1,12 +1,28 @@
 /* eslint-disable */
 import {version} from './package.json';
 import typescript from 'rollup-plugin-typescript2';
+import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-copy';
 
 export default [
     {
         input: './src/PuixDev.ts',
         plugins: [
-            typescript({tsconfig: "tsconfig.development.json"})
+            typescript({tsconfig: "tsconfig.development.json"}),
+            clear({ targets: ['development'] }),
+            copy({
+                targets: [
+                    { 
+                        src: 'assets/index.html',
+                        dest: 'development',
+                        transform: (contents) => contents.toString().replace('{{version}}', version)
+                    },
+                    {
+                        src: ['assets/*.css', 'assets/*.woff', 'assets/*.woff2'],
+                        dest: 'development'
+                    }
+                ]
+            })
         ],
         output: {
             file: './development/esnext.' + version + '.js',
--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,10 +1,12 @@
 import ApplicationElement from './containers/ApplicationElement';
 import ITextElement from './interfaces/ITextElement';
 import TextElement from './text/TextElement';
+import FontDescription from './text/FontDescription';
 import AnchorLayout from './layouts/AnchorLayout';
 import IAnchorLayoutData from './interfaces/IAnchorLayoutData';
 import AnchorLayoutData from './layouts/AnchorLayoutData';
 import FontWeight from './enums/FontWeight';
+import WhiteSpace from './enums/WhiteSpace';
 
 export default class PuixDev extends ApplicationElement {
     public constructor() {
@@ -13,15 +15,27 @@ export default class PuixDev extends ApplicationElement {
         this.layout = new AnchorLayout();
         const textElement: ITextElement = new TextElement();
         textElement.text = 'HÅÆ ØxTX jgq PTH';
-        textElement.fontSize = 32;
-        textElement.truncate = true;
-        textElement.width = 200;
+        textElement.fontSize = 64;
+        // textElement.truncate = true;
+        // textElement.width = 200;
+        textElement.fontDescription = new FontDescription('"Montserrat", sans-serif', 0.7013, -0.0117);
+        textElement.whiteSpace = WhiteSpace.NO_WRAP;
         textElement.fontWeight = FontWeight.BOLD;
         const layoutData: IAnchorLayoutData = new AnchorLayoutData();
         layoutData.horizontalCenter = 0;
-        layoutData.verticalCenter = 0;
+        layoutData.verticalCenter = -50;
         textElement.layoutData = layoutData;
         this.addElement(textElement);
+        const montTextElement: ITextElement = new TextElement();
+        const ldm: IAnchorLayoutData = new AnchorLayoutData();
+        ldm.horizontalCenter = 0;
+        ldm.verticalCenter = 50;
+        montTextElement.text = 'HÅÆ ØxTX jgq PTH';
+        montTextElement.fontSize = 64;
+        montTextElement.layoutData = ldm;
+        montTextElement.whiteSpace = WhiteSpace.NO_WRAP;
+        montTextElement.fontDescription = new FontDescription('"Montserrat", sans-serif', 0.7013, -0.0117);
+        this.addElement(montTextElement);
     }
 }
 customElements.define('puix-dev', PuixDev);
