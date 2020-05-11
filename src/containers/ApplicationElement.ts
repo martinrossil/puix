bab03762717f638af0b427b39318462e90fe9dc9
Martin Rossil <martinrossil@gmail.com>
2020-05-11 10:58:15 +0200

    Switched from enums to strings.

--- src/containers/ApplicationElement.ts
+++ src/containers/ApplicationElement.ts
@@ -1,6 +1,4 @@
 import DisplayContainer from './DisplayContainer';
-import Styles from '../enums/Styles';
-import Values from '../enums/Values';
 import Events from '../enums/Events';
 import Overflow from '../enums/Overflow';
 
@@ -8,9 +6,9 @@ export default class ApplicationElement extends DisplayContainer {
     public constructor() {
         super();
         this.name = 'ApplicationElement';
-        document.body.style.setProperty(Styles.POSITION, Values.ABSOLUTE);
-        document.body.style.setProperty(Styles.WEBKIT_OVERFLOW_SCROLLING, Values.TOUCH);
-        document.body.style.setProperty(Styles.MARGIN, Values.ZERO);
+        document.body.style.setProperty('position', 'absolute');
+        document.body.style.setProperty('-webkit-overflow-scrolling', 'touch');
+        document.body.style.setProperty('margin', '0');
         window.addEventListener(Events.RESIZE, () => {
             this.setSize(window.innerWidth, window.innerHeight);
         });
