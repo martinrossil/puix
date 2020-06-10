d91bca293cf88d14c3101e7a0330c46f47c6b7fa
Martin Rossil <martinrossil@gmail.com>
2020-06-10 23:04:00 +0200

    Created ShapeElement.

--- src/svg/ShapeElement.ts
+++ src/svg/ShapeElement.ts
@@ -1,6 +1,7 @@
 import IShapeElement from '../interfaces/svg/IShapeElement';
 import CornerType from '../consts/CornerType';
 import PathElement from './PathElement';
+import ShapeUtil from './utils/ShapeUtil';
 
 export default class ShapeElement extends PathElement implements IShapeElement {
     public constructor() {
@@ -15,74 +16,12 @@ export default class ShapeElement extends PathElement implements IShapeElement {
 
     protected updatePathData(): void {
         if (this.cornerType === CornerType.NONE) {
-            this.pathData = this.nonePathData;
+            this.pathData = ShapeUtil.getNonePath(this.actualWidth, this.actualHeight);
         } else if (this.cornerType === CornerType.ROUNDED) {
-            this.pathData = this.roundedPathData;
+            this.pathData = ShapeUtil.getRoundedPath(this.actualWidth, this.actualHeight, this.cornerSize);
         } else if (this.cornerType === CornerType.CUT) {
-            this.pathData = this.cutPathData;
+            this.pathData = ShapeUtil.getCutPath(this.actualWidth, this.actualHeight, this.cornerSize);
         }
-    }
-
-    protected get nonePathData(): string {
-        return 'M 0,0 L ' + this.actualWidth + ',0 L ' + this.actualWidth + ',' + this.actualHeight + ' L 0,' + this.actualHeight + ' Z';
-    }
-
-    protected get cutPathData(): string {
-        const tlc = this.cornerSize;
-        const trc = tlc;
-        const brc = tlc;
-        const blc = tlc;
-
-        let d = '';
-        // move to topLeftCorner
-        d += 'M ' + tlc + ',0 ';
-        // line to topRightCorner
-        d += 'L ' + (this.actualWidth - trc) + ',0 ';
-        // cut topRightCorner
-        d += 'L ' + this.actualWidth + ',' + trc + ' ';
-        // line down to bottomRight
-        d += 'L ' + this.actualWidth + ',' + (this.actualHeight - brc) + ' ';
-        // cut bottomRight
-        d += 'L ' + (this.actualWidth - brc) + ',' + this.actualHeight + ' ';
-        // line left to bottomLeft
-        d += 'L ' + blc + ',' + this.actualHeight + ' ';
-        // cut bottomLeft
-        d += 'L 0,' + (this.actualHeight - blc) + ' ';
-        // line up to topLeft
-        d += 'L 0,' + tlc + ' ';
-        // top left cut
-        d += 'L ' + tlc + ',0';
-        // close path
-        d += 'Z';
-        return d;
-    }
-
-    protected get roundedPathData(): string {
-        const tlc = this.cornerSize;
-        const trc = tlc;
-        const brc = tlc;
-        const blc = tlc;
-
-        let d = '';
-        // mov top left arc start
-        d += 'M 0 ' + tlc + ' ';
-        // tlc arc
-        d += 'A ' + tlc + ' ' + tlc + ' 0 0 1 ' + tlc + ' 0 ';
-        // line to topRightCorner
-        d += 'L ' + (this.actualWidth - trc) + ' 0 ';
-        // trc arc
-        d += 'A ' + trc + ' ' + trc + ' 1 0 1 ' + this.actualWidth + ' ' + trc + ' ';
-        // line to bottomRightCorner
-        d += 'L ' + this.actualWidth + ' ' + (this.actualHeight - brc) + ' ';
-        // brc arc
-        d += 'A ' + brc + ' ' + brc + ' 1 0 1 ' + (this.actualWidth - brc) + ' ' + this.actualHeight + ' ';
-        // line to bottomLeftCorner
-        d += 'L ' + blc + ' ' + this.actualHeight + ' ';
-        // blc arc
-        d += 'A ' + blc + ' ' + blc + ' 0 0 1 ' + '0 ' + (this.actualHeight - blc) + ' ';
-        // close path
-        d += 'Z';
-        return d;
     }
 
     private _cornerType = CornerType.NONE;
