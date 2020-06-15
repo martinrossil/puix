6ecf91951203aabad54ae8a7675654beb75df73d
Martin Rossil <martinrossil@gmail.com>
2020-06-15 23:55:24 +0200

    Fixed broken test.

--- tests/IDisplayElement.spec.ts
+++ tests/IDisplayElement.spec.ts
@@ -6,25 +6,10 @@ import DisplayElement from '../src/core/DisplayElement';
 const displayElement: IDisplayElement = new DisplayElement();
 
 beforeEach(() => {
-    displayElement.backgroundColor = '';
     displayElement.opacity = 1;
 });
 
 describe('IDisplayElement interface', () => {
-    describe('default values', () => {
-        it('default backgroundColor should be ""', () => {
-            assert.strictEqual(displayElement.backgroundColor, '');
-        });
-        it('default opacity should be 1', () => {
-            assert.strictEqual(displayElement.opacity, 1);
-        });
-    });
-    describe('backgroundColor', () => {
-        it('given default backgroundColor, when backgroundColor = #CC0000, backgroundColor should be "#CC0000"', () => {
-            displayElement.backgroundColor = '#CC0000';
-            assert.strictEqual(displayElement.backgroundColor, '#CC0000');
-        });
-    });
     describe('opacity', () => {
         it('given opacity is 1, when opacity = -1, opacity should be 0', () => {
             displayElement.opacity = -1;
