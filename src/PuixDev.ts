12f56721cf3d47615ceef2549eeda0cf4dde800c
Martin Rossil <martinrossil@gmail.com>
2020-06-30 23:08:31 +0200

    Added Appbar.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,34 +1,11 @@
 import ApplicationElement from './containers/ApplicationElement';
-import RippleComponent from './components/RippleComponent';
-import Point from './vo/Point';
-import { IPoint } from '.';
-import Events from './consts/Events';
+import Appbar from './components/custom/AppBar';
 
 export default class PuixDev extends ApplicationElement {
     public constructor() {
         super();
         this.name = 'PuixDev';
-        this.addElement(this.rc);
-        this.addEventListener(Events.POINTER_TRIGGERED, this.test as EventListener);
-    }
-
-    protected test(e: CustomEvent<IPoint>): void {
-        console.log('puix', e.type, e.detail, e.detail instanceof Point);
-        this.rc.horizontalCenter = NaN;
-    }
-
-    private _rc!: RippleComponent;
-
-    protected get rc(): RippleComponent {
-        if (!this._rc) {
-            this._rc = new RippleComponent();
-            this._rc.percentWidth = 50;
-            this._rc.percentHeight = 50;
-            // this._rc.setSize(200, 200);
-            this._rc.horizontalCenter = 0;
-            this._rc.verticalCenter = 0;
-        }
-        return this._rc;
+        this.addElement(new Appbar());
     }
 }
 customElements.define('puix-dev', PuixDev);
--- src/components/custom/AppBar.ts
+++ src/components/custom/AppBar.ts
@@ -7,35 +7,73 @@ import TextElement from '../../text/TextElement';
 import HSL from '../../design/color/HSL';
 import HorizontalLayout from '../../layouts/HorizontalLayout';
 import VerticalAlign from '../../consts/VerticalAlign';
+import IShapeElement from '../../interfaces/svg/IShapeElement';
+import ShapeElement from '../../svg/ShapeElement';
+import ShadowFilter from '../../svg/filters/ShadowFilter';
+import IDisplayContainer from '../../interfaces/containers/IDisplayContainer';
+import AnchorLayout from '../../layouts/AnchorLayout';
 
 export default class Appbar extends DisplayContainer {
     public constructor() {
         super();
         this.name = 'AppBar';
-        this.layout = new HorizontalLayout(16, VerticalAlign.MIDDLE);
-        this.layout.paddingLeft = 16;
-        this.z = 2;
+        this.layout = new AnchorLayout();
         this.percentWidth = 100;
         this.height = 64;
-        this.addElement(this.iconElement);
-        this.addElement(this.textElement);
+        this.addElement(this.shapeElement);
+        this.addElement(this.horizontalContainer);
     }
+
+    private _horizontalContainer!: IDisplayContainer;
+
+    protected get horizontalContainer(): IDisplayContainer {
+        if (!this._horizontalContainer) {
+            this._horizontalContainer = new DisplayContainer();
+            this._horizontalContainer.percentWidth = 100;
+            this._horizontalContainer.percentHeight = 100;
+            this._horizontalContainer.layout = new HorizontalLayout(16, VerticalAlign.MIDDLE);
+            this._horizontalContainer.layout.paddingLeft = 16;
+            this._horizontalContainer.addElements([this.iconElement, this.textElement]);
+        }
+        return this._horizontalContainer;
+    }
+
+    private _shapeElement!: IShapeElement;
+
+    protected get shapeElement(): IShapeElement {
+        if (!this._shapeElement) {
+            this._shapeElement = new ShapeElement();
+            this._shapeElement.fillColor = this.theme.colors.secondary.c500;
+            this._shapeElement.percentWidth = 100;
+            this._shapeElement.percentHeight = 100;
+            this._shapeElement.filter = new ShadowFilter(0, 4, 8, '#000', 0.5);
+        }
+        return this._shapeElement;
+    }
+
+    private _iconElement!: IIconElement;
 
     protected get iconElement(): IIconElement {
-        const iconElement: IIconElement = new IconElement();
-        iconElement.setSize(32, 32);
-        iconElement.icon = Icons.PUIX;
-        iconElement.color = this.theme.colors.primary.c500;
-        return iconElement;
+        if (!this._iconElement) {
+            this._iconElement = new IconElement();
+            this._iconElement.setSize(32, 32);
+            this._iconElement.icon = Icons.PUIX;
+            this._iconElement.color = this.theme.colors.primary.c500;
+        }
+        return this._iconElement;
     }
 
+    private _textElement!: ITextElement;
+
     protected get textElement(): ITextElement {
-        const textElement: ITextElement = new TextElement();
-        textElement.text = 'Pui/x';
-        textElement.color = HSL.WHITE;
-        textElement.fontWeight = 700;
-        textElement.fontSize = 24;
-        return textElement;
+        if (!this._textElement) {
+            this._textElement = new TextElement();
+            this._textElement.text = 'Pui/x';
+            this._textElement.color = HSL.WHITE;
+            this._textElement.fontWeight = 700;
+            this._textElement.fontSize = 24;
+        }
+        return this._textElement;
     }
 }
 customElements.define('app-bar', Appbar);
