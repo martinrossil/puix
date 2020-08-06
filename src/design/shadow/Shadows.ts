d4d597f0cead00449d92acc3a1184aca245bb477
Martin Rossil <martinrossil@gmail.com>
2020-08-06 23:26:40 +0200

    Changes shadows.

--- src/design/shadow/Shadows.ts
+++ src/design/shadow/Shadows.ts
@@ -3,9 +3,9 @@ import IShadows from '../../interfaces/design/shadow/IShadows';
 export default class Shadows implements IShadows {
     public none = 'none';
     public inner = 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)';
-    public xSmall = '0 1px 2px 0 rgba(0, 0, 0, 0.05)';
-    public small = '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)';
-    public medium = '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)';
-    public large = '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)';
-    public xLarge = '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)';
+    public xSmall = '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'; // '0 1px 2px 0 rgba(0, 0, 0, 0.05)';
+    public small = '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'; // '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)';
+    public medium = '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'; // '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)';
+    public large = '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'; // '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)';
+    public xLarge = '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'; // '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)';
 }
