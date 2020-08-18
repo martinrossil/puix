d8dfe1110b0bd1d5c2f6cfd078f8bc1547e61e17
Martin Rossil <martinrossil@gmail.com>
2020-08-18 23:31:55 +0200

    Deleted components/custom.

--- src/components/ButtonComponentInterface.ts
+++
@@ -1,9 +0,0 @@
-import DisplayContainerInterface from '../containers/DisplayContainerInterface';
-
-export default interface ButtonComponentInterface extends DisplayContainerInterface {
-    label: string;
-    icon: string;
-    labelColor: string;
-    buttonType: string;
-    iconSize: number;
-}
