bc6daed2b28986b26a5db8e98584ec7040c4b0d9
Martin Rossil <martinrossil@gmail.com>
2020-08-18 23:57:35 +0200

    Updated karma.config.

--- karma.conf.js
+++ karma.conf.js
@@ -47,6 +47,7 @@ module.exports = function karma(config) {
             },
             exclude: [
                 'node_modules',
+                'dist',
                 '*spec.ts',
             ],
             coverageOptions: {
