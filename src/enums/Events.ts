58e2e1f228c0c49f033bf60731115ec85497ee7a
Martin Rossil <martinrossil@gmail.com>
2020-10-21 16:32:21 +0200

    Added ITEM event types.

--- src/enums/Events.ts
+++ src/enums/Events.ts
@@ -1,5 +1,9 @@
 export const enum Events {
     INTERNAL_SIZE_CHANGED = 'a',
     APPLICATION_COMPLETE = 'b',
-    LAYOUT_DATA_CHANGED = 'c'
+    LAYOUT_DATA_CHANGED = 'c',
+    ITEM_ADDED = 'd',
+    ITEM_REMOVED = 'e',
+    ITEMS_ADDED = 'f',
+    RESET = 'g'
 }
