e943a0b1c57c1e34834a3a8cf8e83a84b944718d
Martin Rossil <martinrossil@gmail.com>
2020-10-21 11:27:18 +0200

    Created IApplicationElement test.

---
+++ tests/IApplicationElement.spec.ts
@@ -0,0 +1,40 @@
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import { ApplicationElement, Events, Overflow } from '../src';
+import IApplicationElement from '../src/interfaces/containers/IApplicationElement';
+
+const applicationElement: IApplicationElement = new ApplicationElement();
+
+describe('IApplicationElement interface', () => {
+    describe('default values', () => {
+        it('actual size should be same as window inner size', () => {
+            assert.strictEqual(applicationElement.actualWidth, window.innerWidth);
+            assert.strictEqual(applicationElement.actualHeight, window.innerHeight);
+        });
+        it('overflow should be ' + Overflow.HIDDEN, () => {
+            assert.strictEqual(applicationElement.overflow, Overflow.HIDDEN);
+        });
+        it('backgroundColor should be ""', () => {
+            assert.strictEqual(applicationElement.backgroundColor, '');
+        });
+    });
+    describe('dispatch APPLICATION_COMPLETE', () => {
+        it('given applicationElement has been added to DOM, APPLICATION_COMPLETE event should be dispatched', () => {
+            let hasBeenDispatched = false;
+            applicationElement.addEventListener(Events.APPLICATION_COMPLETE, () => {
+                hasBeenDispatched = true;
+            });
+            document.body.appendChild(applicationElement as unknown as Node);
+            assert.isTrue(hasBeenDispatched);
+        });
+    });
+    describe('backgroundColor', () => {
+        it('given backgroundColor = "red", backgroundColor should be "red"', () => {
+            applicationElement.backgroundColor = 'red';
+            assert.strictEqual(applicationElement.backgroundColor, 'red');
+        });
+        it('given backgroundColor is "red", document.body should be "red"', () => {
+            assert.strictEqual(document.body.style.getPropertyValue('background-color'), 'red');
+        });
+    });
+});
