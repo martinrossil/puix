a62eace7ab6069e56ab4a4882b9450df9d788c07
Martin Rossil <martinrossil@gmail.com>
2020-10-23 13:13:17 +0200

    Removed double quotes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        "ecmaVersion": 11,
-        "sourceType": "module",
+        ecmaVersion: 11,
+        sourceType: 'module'
     },
-    ignorePatterns: ["dist/", "development/"],
+    ignorePatterns: ['dist/', 'development/'],
     env: {
-        "browser": true, // makes HTMLElement and customElements NOT no-undef
+        browser: true // makes HTMLElement and customElements NOT no-undef
     },
     plugins: ['@typescript-eslint'],
     extends: [
@@ -14,25 +14,25 @@ module.exports = {
         'standard'
     ],
     rules: {
-        "prefer-template": "off",
-        "no-underscore-dangle": "off",
-        "linebreak-style": "off",
-        "no-console": "off",
-        "@typescript-eslint/indent": "off",
-        "class-methods-use-this": "off",
-        "import/no-duplicates": "off",
-        "import/prefer-default-export": "off",
-        "max-len": "off",
-        "import/no-extraneous-dependencies": "off",
-        "indent": "off",
-        "semi": "off",
-        "space-before-function-paren": "off",
-        "@typescript-eslint/no-useless-constructor": "off",
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        // "@typescript-eslint/no-unused-vars": "off",
-        "no-useless-constructor": "off",
-        "no-restricted-syntax": "off", // so we can use for of loop instead if Array iterator
-        "no-plusplus": "off"
+        'prefer-template': 'off',
+        'no-underscore-dangle': 'off',
+        'linebreak-style': 'off',
+        'no-console': 'off',
+        '@typescript-eslint/indent': 'off',
+        'class-methods-use-this': 'off',
+        'import/no-duplicates': 'off',
+        'import/prefer-default-export': 'off',
+        'max-len': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        indent: 'off',
+        semi: 'off',
+        'space-before-function-paren': 'off',
+        '@typescript-eslint/no-useless-constructor': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        // '@typescript-eslint/no-unused-vars': 'off',
+        'no-useless-constructor': 'off',
+        'no-restricted-syntax': 'off', // so we can use for of loop instead if Array iterator
+        'no-plusplus': 'off'
     }
 }
