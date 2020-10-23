b93fdf4c6deec6dfe27d7c82f160f60eabdc288c
Martin Rossil <martinrossil@gmail.com>
2020-10-23 11:29:00 +0200

    Changed Interfaces.

--- src/layouts/BaseLayout.ts
+++ src/layouts/BaseLayout.ts
@@ -19,36 +19,36 @@ export default class BaseLayout implements ILayout {
         this.layoutElements(container, elements);
     }
 
-    private hasExplicitSize(container: IDisplayContainer): boolean {
-        return this.hasExplicitWidth(container) && this.hasExplicitHeight(container);
+    private hasExplicitSize(element: ILayoutElement): boolean {
+        return this.hasExplicitWidth(element) && this.hasExplicitHeight(element);
     }
 
-    private hasExplicitWidth(container: IDisplayContainer): boolean {
-        if (!isNaN(container.width)) {
+    private hasExplicitWidth(element: ILayoutElement): boolean {
+        if (!isNaN(element.width)) {
             return true;
         }
-        if (!isNaN(container.percentWidth)) {
+        if (!isNaN(element.percentWidth)) {
             return true;
         }
-        if (!isNaN(container.left) && !isNaN(container.right)) {
+        if (!isNaN(element.left) && !isNaN(element.right)) {
             return true;
         }
-        const node: Node = container as unknown as Node;
+        const node: Node = element as unknown as Node;
         const parent: IDisplayContainer = node.parentNode as unknown as IDisplayContainer;
         return parent.horizontalAlign === HorizontalAlign.FILL;
     }
 
-    private hasExplicitHeight(container: IDisplayContainer): boolean {
-        if (!isNaN(container.height)) {
+    private hasExplicitHeight(element: ILayoutElement): boolean {
+        if (!isNaN(element.height)) {
             return true;
         }
-        if (!isNaN(container.percentHeight)) {
+        if (!isNaN(element.percentHeight)) {
             return true;
         }
-        if (!isNaN(container.top) && !isNaN(container.bottom)) {
+        if (!isNaN(element.top) && !isNaN(element.bottom)) {
             return true;
         }
-        const node: Node = container as unknown as Node;
+        const node: Node = element as unknown as Node;
         const parent: IDisplayContainer = node.parentNode as unknown as IDisplayContainer;
         return parent.verticalAlign === VerticalAlign.FILL;
     }
