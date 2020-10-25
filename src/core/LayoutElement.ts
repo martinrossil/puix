ae14508751bb953b7e9c4d68e6a060513828a2a3
Martin Rossil <martinrossil@gmail.com>
2020-10-25 13:42:37 +0100

    Refactored to return early.

--- src/core/LayoutElement.ts
+++ src/core/LayoutElement.ts
@@ -14,10 +14,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._top) && isNaN(value)) {
             return;
         }
-        if (this._top !== value) {
-            this._top = value;
-            this.notifyLayoutDataChanged();
+        if (this._top === value) {
+            return;
         }
+        this._top = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get top(): number {
@@ -30,10 +31,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._right) && isNaN(value)) {
             return;
         }
-        if (this._right !== value) {
-            this._right = value;
-            this.notifyLayoutDataChanged();
+        if (this._right === value) {
+            return;
         }
+        this._right = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get right(): number {
@@ -46,10 +48,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._bottom) && isNaN(value)) {
             return;
         }
-        if (this._bottom !== value) {
-            this._bottom = value;
-            this.notifyLayoutDataChanged();
+        if (this._bottom === value) {
+            return;
         }
+        this._bottom = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get bottom(): number {
@@ -62,10 +65,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._left) && isNaN(value)) {
             return;
         }
-        if (this._left !== value) {
-            this._left = value;
-            this.notifyLayoutDataChanged();
+        if (this._left === value) {
+            return;
         }
+        this._left = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get left(): number {
@@ -78,10 +82,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._horizontalCenter) && isNaN(value)) {
             return;
         }
-        if (this._horizontalCenter !== value) {
-            this._horizontalCenter = value;
-            this.notifyLayoutDataChanged();
+        if (this._horizontalCenter === value) {
+            return;
         }
+        this._horizontalCenter = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get horizontalCenter(): number {
@@ -94,10 +99,11 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._verticalCenter) && isNaN(value)) {
             return;
         }
-        if (this._verticalCenter !== value) {
-            this._verticalCenter = value;
-            this.notifyLayoutDataChanged();
+        if (this._verticalCenter === value) {
+            return;
         }
+        this._verticalCenter = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get verticalCenter(): number {
@@ -116,22 +122,23 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._percentWidth) && isNaN(value)) {
             return;
         }
-        if (this._percentWidth !== value) {
-            if (!isNaN(value)) {
-                if (value < 0) {
-                    if (this._percentWidth !== 0) {
-                        this._percentWidth = 0;
-                        this.notifyLayoutDataChanged();
-                    }
-                } else {
-                    this._percentWidth = value;
-                    this.notifyLayoutDataChanged();
-                }
-            } else {
-                this._percentWidth = value;
+        if (this._percentWidth === value) {
+            return;
+        }
+        if (isNaN(value)) {
+            this._percentWidth = value;
+            this.notifyLayoutDataChanged();
+            return;
+        }
+        if (value < 0) {
+            if (this._percentWidth !== 0) {
+                this._percentWidth = 0;
                 this.notifyLayoutDataChanged();
             }
+            return;
         }
+        this._percentWidth = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get percentWidth(): number {
@@ -144,22 +151,23 @@ export default class LayoutElement extends SizeElement implements ILayoutElement
         if (isNaN(this._percentHeight) && isNaN(value)) {
             return;
         }
-        if (this._percentHeight !== value) {
-            if (!isNaN(value)) {
-                if (value < 0) {
-                    if (this._percentHeight !== 0) {
-                        this._percentHeight = 0;
-                        this.notifyLayoutDataChanged();
-                    }
-                } else {
-                    this._percentHeight = value;
-                    this.notifyLayoutDataChanged();
-                }
-            } else {
-                this._percentHeight = value;
+        if (this._percentHeight === value) {
+            return;
+        }
+        if (isNaN(value)) {
+            this._percentHeight = value;
+            this.notifyLayoutDataChanged();
+            return;
+        }
+        if (value < 0) {
+            if (this._percentHeight !== 0) {
+                this._percentHeight = 0;
                 this.notifyLayoutDataChanged();
             }
+            return;
         }
+        this._percentHeight = value;
+        this.notifyLayoutDataChanged();
     }
 
     public get percentHeight(): number {
