72dab6dc7b8b75f0f03edb6618a1643e6394ca78
Martin Rossil <martinrossil@gmail.com>
2020-10-29 12:05:55 +0100

    Added more tests.

--- tests/IEventDispatcher.spec.ts
+++ tests/IEventDispatcher.spec.ts
@@ -2,53 +2,148 @@ import { assert } from 'chai';
 import { describe, it } from 'mocha';
 import { EventDispatcher, EventDispatcherElement, IEventDispatcher, IEventListener } from '../src';
 
-let handlerHasBeenCalled = false;
-let handlerHasBeenCalled2 = false;
-const handler: IEventListener = () => {
-    handlerHasBeenCalled = true;
-};
-const handler2: IEventListener = () => {
-    handlerHasBeenCalled2 = true;
-};
-const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
-eventDispatcherElement.addEventListener('test', handler);
-
-const eventDispatcher: IEventDispatcher = new EventDispatcher();
-eventDispatcher.addEventListener('test', handler2);
-
 describe('IEventDispatcher interface', () => {
     describe('default values', () => {
         it('default name should be ""', () => {
+            const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
             assert.strictEqual(eventDispatcherElement.name, '');
             assert.strictEqual(eventDispatcher.name, '');
         });
     });
     describe('name', () => {
         it('given name is "", when name = "ComponentName", name should be "ComponentName"', () => {
+            const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
             eventDispatcherElement.name = 'ComponentName';
             eventDispatcher.name = 'ComponentName';
             assert.strictEqual(eventDispatcherElement.name, 'ComponentName');
             assert.strictEqual(eventDispatcher.name, 'ComponentName');
         });
+        it('given name is "", when name = "", name should be ""', () => {
+            const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcherElement.name = '';
+            eventDispatcher.name = '';
+            assert.strictEqual(eventDispatcherElement.name, '');
+            assert.strictEqual(eventDispatcher.name, '');
+        });
     });
     describe('addEventListener()', () => {
         it('given addEventListener(), when dispatchEventWith("type"), handler should have been called', () => {
+            const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            let handlerHasBeenCalled = false;
+            let handlerHasBeenCalled2 = false;
+            eventDispatcherElement.addEventListener('test', () => {
+                handlerHasBeenCalled = true;
+            });
+            eventDispatcher.addEventListener('test', () => {
+                handlerHasBeenCalled2 = true;
+            });
             eventDispatcherElement.dispatchEventWith('test', this, false);
             eventDispatcher.dispatchEventWith('test', this, false);
+            assert.isTrue(handlerHasBeenCalled);
+            assert.isTrue(handlerHasBeenCalled2);
+        });
+        it('given addEventListener() is called twice, addEventListener should be covered', () => {
+            let handlerHasBeenCalled = false;
+            let handlerHasBeenCalled2 = false;
+            const eventHandler: IEventListener = () => {
+                handlerHasBeenCalled = true;
+            };
+            const eventHandler2: IEventListener = () => {
+                handlerHasBeenCalled2 = true;
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.addEventListener('test', eventHandler);
+            eventDispatcher.addEventListener('test', eventHandler2);
+            eventDispatcher.dispatchEvent(new Event('test'));
+            eventDispatcher.dispatchEvent(new Event('test'));
             assert.isTrue(handlerHasBeenCalled);
             assert.isTrue(handlerHasBeenCalled2);
         });
     });
     describe('removeEventListener()', () => {
         it('given removeEventListener(), when dispatchEventWith("type"), handler should NOT have been called', () => {
-            handlerHasBeenCalled = false;
-            handlerHasBeenCalled2 = false;
-            eventDispatcher.removeEventListener('test', handler2);
-            eventDispatcherElement.removeEventListener('test', handler);
+            const eventDispatcherElement: IEventDispatcher = new EventDispatcherElement();
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            let handlerHasBeenCalled = false;
+            let handlerHasBeenCalled2 = false;
+            const eventHandler: IEventListener = () => {
+                handlerHasBeenCalled = true;
+            };
+            const eventHandler2: IEventListener = () => {
+                handlerHasBeenCalled2 = true;
+            };
+            eventDispatcherElement.addEventListener('test', eventHandler);
+            eventDispatcher.addEventListener('test', eventHandler2);
+            eventDispatcherElement.removeEventListener('test', eventHandler);
+            eventDispatcher.removeEventListener('test', eventHandler2);
             eventDispatcherElement.dispatchEventWith('test', this, false);
             eventDispatcher.dispatchEventWith('test', this, false);
             assert.isFalse(handlerHasBeenCalled);
             assert.isFalse(handlerHasBeenCalled2);
+        });
+        it('given addEventListener() is called twice, removeEventListener() twice should be covered', () => {
+            let handlerHasBeenCalled = false;
+            let handlerHasBeenCalled2 = false;
+            const eventHandler: IEventListener = () => {
+                handlerHasBeenCalled = true;
+            };
+            const eventHandler2: IEventListener = () => {
+                handlerHasBeenCalled2 = true;
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.addEventListener('test', eventHandler);
+            eventDispatcher.addEventListener('test', eventHandler2);
+            eventDispatcher.removeEventListener('test', eventHandler);
+            eventDispatcher.removeEventListener('test', eventHandler2);
+            assert.isFalse(handlerHasBeenCalled);
+            assert.isFalse(handlerHasBeenCalled2);
+        });
+        it('given removeEventListener() with unknown listener', () => {
+            let handlerHasBeenCalled = false;
+            const eventHandler: IEventListener = () => {
+                handlerHasBeenCalled = true;
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.removeEventListener('test', eventHandler);
+            assert.isFalse(handlerHasBeenCalled);
+        });
+        it('given removeEventListener() with known and unknown listener', () => {
+            let handlerHasBeenCalled = false;
+            let handlerHasBeenCalled2 = false;
+            const eventHandler: IEventListener = () => {
+                handlerHasBeenCalled = true;
+            };
+            const eventHandler2: IEventListener = () => {
+                handlerHasBeenCalled2 = true;
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.addEventListener('test', eventHandler);
+            eventDispatcher.removeEventListener('test', eventHandler2);
+            eventDispatcher.removeEventListener('test', eventHandler);
+            assert.isFalse(handlerHasBeenCalled);
+            assert.isFalse(handlerHasBeenCalled2);
+        });
+    });
+    describe('dispatchEvent()', () => {
+        it('given there are listeners, dispatchEvent should cover typeListeners !== undefined', () => {
+            const listener: IEventListener = () => {
+                console.log('listener triggered');
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.addEventListener('test', listener);
+            eventDispatcher.dispatchEvent(new Event('test'));
+        });
+        it('given there are listeners, dispatchEvent with unknown type should cover else clause', () => {
+            const listener: IEventListener = () => {
+                console.log('listener triggered');
+            };
+            const eventDispatcher: IEventDispatcher = new EventDispatcher();
+            eventDispatcher.addEventListener('test', listener);
+            eventDispatcher.dispatchEvent(new Event('no_type'));
         });
     });
 });
--- tests/ITextElement.spec.ts
+++ tests/ITextElement.spec.ts
@@ -1,12 +1,16 @@
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
-import { ITextElement, TextElement } from '../src';
+import { DisplayContainer, IDisplayContainer, ITextElement, ITypeFace, TextElement } from '../src';
 
 describe('ITextElement', () => {
     describe('default values', () => {
         it('verticalGap should be 4', () => {
             const textElement: ITextElement = new TextElement();
             assert.strictEqual(textElement.verticalGap, 4);
+        });
+        it('typeFace should not be null', () => {
+            const textElement: ITextElement = new TextElement();
+            assert.isNotNull(textElement.typeFace);
         });
     });
     describe('verticalGap', () => {
@@ -24,6 +28,87 @@ describe('ITextElement', () => {
             const textElement: ITextElement = new TextElement();
             textElement.verticalGap = NaN;
             assert.strictEqual(textElement.verticalGap, 4);
+        });
+    });
+    describe('typeFace', () => {
+        it('given typeFace is not null, when typeFace = typeFace, set should return early', () => {
+            const textElement: ITextElement = new TextElement();
+            const typeFace: ITypeFace = textElement.typeFace;
+            textElement.typeFace = typeFace;
+        });
+    });
+    describe('protected hasExplicitWidth', () => {
+        it('given IBaseText width is 100, hasExplicitWidth should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.width = 100;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.width, 100);
+        });
+        it('given IBaseText percentWidth is 100, hasExplicitWidth should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.percentWidth = 100;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.percentWidth, 100);
+        });
+        it('given IBaseText left and right is 20, hasExplicitWidth should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.left = 20;
+            textElement.right = 20;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.left, 20);
+            assert.strictEqual(textElement.right, 20);
+        });
+    });
+    describe('protected hasExplicitHeight', () => {
+        it('given IBaseText height is 100, hasExplicitHeight should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.height = 100;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.height, 100);
+        });
+        it('given IBaseText percentHeight is 100, hasExplicitHeight should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.percentHeight = 100;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.percentHeight, 100);
+        });
+        it('given IBaseText top and bottom is 20, hasExplicitHeight should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.top = 20;
+            textElement.bottom = 20;
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.top, 20);
+            assert.strictEqual(textElement.bottom, 20);
+        });
+    });
+    describe('protected hasExplicitSize', () => {
+        it('given IBaseText size is 100, 100, hasExplicitSize should return true', () => {
+            const container: IDisplayContainer = new DisplayContainer();
+            const textElement: ITextElement = new TextElement();
+            textElement.setSize(100, 100);
+            textElement.text = 'Lorem';
+            container.addElement(textElement);
+            document.body.appendChild(container as unknown as Node);
+            assert.strictEqual(textElement.width, 100);
+            assert.strictEqual(textElement.height, 100);
         });
     });
 });
