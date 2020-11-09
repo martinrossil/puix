e7f3e1d9d63c2131027ea907c405a5476af6c609
Martin Rossil <martinrossil@gmail.com>
2020-11-09 10:43:14 +0100

    Removed readonly dataProvider.

--- src/interfaces/containers/IListElement.ts
+++ src/interfaces/containers/IListElement.ts
@@ -3,8 +3,8 @@ import IItemRenderer from '../data/IItemRenderer';
 import IScrollContainer from './IScrollContainer';
 
 export default interface IListElement<Item> extends IScrollContainer {
-    readonly dataProvider: IArrayList<Item>;
+    dataProvider: IArrayList<Item> | null;
     ItemRenderer: new () => IItemRenderer<Item>;
-    readonly selectedItem: Item | null;
     selectedIndex: number;
+    readonly selectedItem: Item | null;
 }
