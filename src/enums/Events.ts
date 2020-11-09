b190d3a498305d792528b851008686c77917f32f
Martin Rossil <martinrossil@gmail.com>
2020-11-09 10:41:28 +0100

    Moved Event type to Dispatchers.

--- src/enums/Events.ts
+++ src/enums/Events.ts
@@ -1,5 +1,4 @@
 export const enum Events {
-    CLICK = 'click',
     INTERNAL_SIZE_CHANGED = 'a',
     APPLICATION_COMPLETE = 'b',
     LAYOUT_DATA_CHANGED = 'c',
@@ -7,7 +6,6 @@ export const enum Events {
     ITEM_REMOVED = 'e',
     ITEMS_ADDED = 'f',
     RESET = 'g',
-    STATE_CHANGED = 'h',
     LIST_ITEM_CLICKED = 'i',
-    SELECTED_INDEX_CHANGED = 'j'
+    TRIGGERED = 'k'
 }
