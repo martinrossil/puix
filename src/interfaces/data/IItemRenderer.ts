17f5a4d2cb54e5f3936baf4f7b241aad9d1af70c
Martin Rossil <martinrossil@gmail.com>
2020-11-09 10:43:55 +0100

    Added readonly currentState.

--- src/interfaces/data/IItemRenderer.ts
+++ src/interfaces/data/IItemRenderer.ts
@@ -1,6 +1,8 @@
 import IDisplayContainer from '../containers/IDisplayContainer';
+import IState from '../fsm/IState';
 
 export default interface IItemRenderer<Item> extends IDisplayContainer {
     data: Item | null;
     selected: boolean;
+    readonly currentState: IState;
 }
