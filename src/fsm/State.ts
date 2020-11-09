dd806f1b93e7e45509e42235f74eabf92f7fdaa6
Martin Rossil <martinrossil@gmail.com>
2020-11-09 10:42:44 +0100

    Added on listener and renamed.

--- src/fsm/State.ts
+++ src/fsm/State.ts
@@ -1,12 +1,12 @@
-import IEnterState from '../interfaces/fsm/IEnterState';
-import IExitState from '../interfaces/fsm/IExitState';
+import IEventListener from '../interfaces/events/IEventListener';
 import IState from '../interfaces/fsm/IState';
 
 export default class State implements IState {
     public name: string;
     public event: Event | null = null;
-    public enterState: IEnterState | null = null;
-    public exitState: IExitState | null = null;
+    public enter: IEventListener | null = null;
+    public on: IEventListener | null = null;
+    public exit: IEventListener | null = null;
     protected targets: Map<string, IState> = new Map();
     public constructor(name: string) {
         this.name = name;
